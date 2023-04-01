5383fcfd53d5266cadb1a95760314bc7dc1ed90b
janson11 <SJ520zxy=1314>
2023-04-01 15:35:37 +0800

    init

---
+++ src/test/java/com/janson/persistence/PersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janson.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
