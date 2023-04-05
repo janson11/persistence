c119035b8793b0482beb5547f3b02c52db7918a7
janson11 <SJ520zxy=1314>
2023-04-06 00:28:05 +0800

    proxy

--- src/main/java/com/janson/sqlSession/DefaultSqlSession.java
+++ src/main/java/com/janson/sqlSession/DefaultSqlSession.java
@@ -3,6 +3,11 @@ package com.janson.sqlSession;
 import com.janson.pojo.Configuration;
 import com.janson.pojo.MappedStatement;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Proxy;
+import java.lang.reflect.Type;
 import java.util.List;
 
 /**
@@ -37,5 +42,36 @@ public class DefaultSqlSession implements SqlSession {
         } else {
             throw new RuntimeException("查询结果为空或者返回结果过多");
         }
+    }
+
+    @Override
+    public <T> T getMapper(Class<?> mapperClass) {
+        // 使用JDK动态代理为Dao接口生成代理对象，并返回
+        Object proxyInstance = Proxy.newProxyInstance(DefaultSqlSession.class.getClassLoader(), new Class[]{mapperClass}, new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                // proxy 当前代理对象的引用。
+                // method :当前被道义方法的引用
+                // args: 传递参数
+                // 底层都还是去执行JDBC代码
+                // 根据不同情况，来调用selectList或selectOne
+                // 准备参数 1：statementId :sql语句的唯一标识：namespace+id = 接口全限定名.方法名
+                // 方法名
+                String methodName = method.getName();
+                String className = method.getDeclaringClass().getName();
+                String statementId = className + "." + methodName;
+                // 准备参数2 ：params:args
+
+                // 获取被调用方法的返回值类型
+                Type genericReturnType = method.getGenericReturnType();
+                // 判断是否进行了泛型类型参数化
+                if (genericReturnType instanceof ParameterizedType) {
+                    List<Object> objects = selectList(statementId, args);
+                    return objects;
+                }
+                return selectOne(statementId, args);
+            }
+        });
+        return (T) proxyInstance;
     }
 }
--- src/main/java/com/janson/sqlSession/SqlSession.java
+++ src/main/java/com/janson/sqlSession/SqlSession.java
@@ -12,10 +12,12 @@ public interface SqlSession {
     /**
      * 查询所有
      */
-    public <E> List<E> selectList(String statementId,Object... params) throws Exception;
+    public <E> List<E> selectList(String statementId, Object... params) throws Exception;
 
     /**
      * 根据条件查询单个
      */
-    public <T> T selectOne(String statementId,Object... params) throws Exception;
+    public <T> T selectOne(String statementId, Object... params) throws Exception;
+
+    public <T> T getMapper(Class<?> mapperClass);
 }
